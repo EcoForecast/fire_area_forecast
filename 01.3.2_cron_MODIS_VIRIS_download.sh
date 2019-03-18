57a13dd4f65730824ca2dc248a0946eba295ad84
shijuan.chen32 <shijuan.chen32@gmail.com>
2019-03-18 17:02:26 -0400

    change the script name in bash script

---
+++ 01.3.2_cron_MODIS_VIRIS_download.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -l
+#$ -V
+#$ -l h_rt=96:00:00
+#$ -N data_download
+#$ -j y
+#$ -l mem_total=98G
+#$ -l mem_per_core=8G
+#$ -pe omp 8
+
+Rscript /usr3/graduate/tmccabe/mccabete/Fire_forecast_509/scripts/fire_area_forecast/01.1_download_MOD_VIIRS.R
