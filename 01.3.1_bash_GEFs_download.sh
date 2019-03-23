1ad78f0c5b1dc709c7ca48cbe7fd5772d40dd001
shijuan.chen32 <shijuan.chen32@gmail.com>
2019-03-23 10:15:53 -0400

    update script

--- 01.3.1_bash_GEFs_download.sh
+++ 01.3.1_bash_GEFs_download.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -l
 #$ -V
 #$ -l h_rt=96:00:00
-#$ -N GEFs_download_cron
+#$ -N data_download
 #$ -j y
 #$ -l mem_total=98G
 #$ -l mem_per_core=8G
 #$ -pe omp 8
 
-Rscript /usr3/graduate/tmccabe/mccabete/Fire_forecast_509/scripts/fire_area_forecast/01.2_GEFs_download.R
+module purge
+module load R/R-3.1.1
+Rscript /usr3/graduate/shijuan/Desktop/fire_area_forecast/01.2_GEFs_download.R
