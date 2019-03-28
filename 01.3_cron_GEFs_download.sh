017283718736d01e0412b8f3c467b806fde21f8d
Tess McCabe <tmccabe@bu.edu>
2019-03-28 17:07:48 -0400

    Loading correct modules

--- 01.3_cron_GEFs_download.sh
+++ 01.3_cron_GEFs_download.sh
@@ -6,5 +6,7 @@
 #$ -l mem_total=98G
 #$ -l mem_per_core=8G
 #$ -pe omp 8
+module load R/3.4.2
+module load udunits/2.2.20
 
 Rscript /usr3/graduate/tmccabe/mccabete/Fire_forecast_509/scripts/fire_area_forecast/01.2_GEFs_download.R
