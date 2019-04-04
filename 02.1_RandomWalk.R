4100428481e854b5a7cce654bbeafe4f195b45cc
Tess McCabe <tmccabe@bu.edu>
2019-04-04 15:29:57 -0400

    actually a dpois works better (does not retunr node errors). Also fixed indexing bug

--- 02.1_RandomWalk.R
+++ 02.1_RandomWalk.R
@@ -4,22 +4,23 @@ Fire_timeseries <-" model {
  ### priors
  beta_precip ~ dnorm(r_0, v_r)
  beta_temp ~ dnorm(k_0, v_k)
- beta_evi ~ dnorm(e_0, es_0)
+ #beta_evi ~ dnorm(e_0, es_0)
  #beta_IC ~ dnorm(I_0, S_0)
  tau_modis ~ dgamma(mod_1, mod_2)
- sigma ~dgamma(s_1, s_2)
+ #sigma ~dgamma(s_1, s_2)
  x[1] ~ dnorm(mu1, v_0)
  
  
  ###Observation error
- for ( i in 2:N){
+ for ( i in 1:N){
  y_modis[i] ~ dnorm(x[i], tau_modis) # modis
  }
  
  ### Process model for Fire
  for(t in 2:N) {
  mu[t] <- x[t-1]+ beta_precip*y[t] + beta_temp*y_2[t] #+beta_IC
- x[t] ~ dlnorm(mu[t],sigma) 
+ theta[t] <- log(mu[t])
+ x[t] ~ dpois(theta[t]) 
  }
  }
  "
@@ -28,8 +29,8 @@ Fire_timeseries <-" model {
  data<-list()
  data$y <- rnorm(10, 10) # Precip
  data$y_2 <- rnorm(10, 200) # Temp
- data$y_3 <- rnorm(10, 10) # Evi
- data$y_modis <- rnorm(10, 50) # modis 
+ #data$y_3 <- rnorm(10, 10) # Evi
+ data$y_modis <- rnorm(10, 50000) # modis 
  data$N<- 10
  
  ### Priors
@@ -37,10 +38,10 @@ Fire_timeseries <-" model {
  data$v_r<- 1/10 ## Unsure how much uncertainty 
  data$k_0<- 3 # Probably positive relationship with temperature
  data$v_k<- 0.5 # no large SD
- data$e_0 <- 0 ## don't know if postive or negative relationship with EVI
- data$es_0 <- 1/100 # large uncertianty
- data$s_1<-2 
- data$s_2<-2
+ #data$e_0 <- 0 ## don't know if postive or negative relationship with EVI
+ #data$es_0 <- 1/100 # large uncertianty
+ #data$s_1<-2 
+ #data$s_2<-2
  data$mu1<-11517700  # I just mulitplied the area of park by 0.01. could change to actual area burned in forrest in previous years
  data$v_0<- 1/3000000  # Make sure varience is roughly max the area of the park (1151770000 m^2), and min 0
  data$mod_1 <- 1
