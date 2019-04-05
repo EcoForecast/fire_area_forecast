61e48d290797290a31933b10d1c20acbc2d9764a
Tess McCabe <tmccabe@bu.edu>
2019-04-05 09:11:00 -0400

    fix the modules loaded for GEFs download

--- 01.3.1_bash_GEFs_download.sh
+++ 01.3.1_bash_GEFs_download.sh
@@ -7,6 +7,7 @@
 #$ -l mem_per_core=8G
 #$ -pe omp 8
 
-module purge
-module load R
+module load R/3.4.2
+module load udunits/2.2.20
+
 Rscript /usr3/graduate/shijuan/Desktop/fire_area_forecast/01.2_GEFs_download.R
