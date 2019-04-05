341f9fb30752d53aaf937d09724e163920b99ce7
shijuanchen <shijuan.chen32@gmail.com>
2019-04-04 21:18:06 -0400

    read data from csv

--- 02.1_RandomWalk.R
+++ 02.1_RandomWalk.R
@@ -2,11 +2,17 @@ library(rjags)
 
 ## Reading in data
 GEFs_data_path <- "/Users/shijuanchen/Desktop/Spring_2019/GE585_Ecological_Forecasting/my_own_fork/fire_area_forecast/summary_data_8days.csv"
-met <- read.csv(GEFs_data_path)
-
+met <- read.csv(GEFs_data_path, header=FALSE)
+met_cp = met[complete.cases(met),]
+date_g = met_cp[1:2,2]
+temp = met_cp[1:2,3]
+precip = met_cp[1:2,4]
 MFire_data_path <- "/Users/shijuanchen/Desktop/Spring_2019/GE585_Ecological_Forecasting/my_own_fork/fire_area_forecast/MODIS_FireProduct.csv"
 mfire <- read.csv(MFire_data_path)
-
+date_m = mfire[5:6,2]
+mfire_cp = mfire[complete.cases(mfire),]
+mf = mfire_cp[5:6,3]
+#mmodis_fire <- []
 Fire_timeseries <-" model {
 
  ### priors
@@ -39,11 +45,11 @@ Fire_timeseries <-" model {
  
  ### data
  data<-list()
- data$y <- c(1.21982473898761e-05, 5.98551858393494e-05, 2.05873840757743e-05) # Precip
- data$y_2 <- c(303.670013427734, 300.600006103516, 298.700012207031) # Temp
+ data$y <- precip # Precip
+ data$y_2 <- temp # Temp
  #data$y_3 <- rnorm(10, 10) # Evi
- data$y_modis <- c(2.45e+08, 2.54e+08) # modis 
- data$N<- 10
+ data$y_modis <- mf # modis 
+ data$N<- 2
  
  ### Priors
  data$r_0<- -3 ## Probably a negative relationship -- likely more influential than temperature 
