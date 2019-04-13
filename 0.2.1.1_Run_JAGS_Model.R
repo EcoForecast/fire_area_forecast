50327b3e9ec50a1778898b837840f8d723f27d03
Tempest McCabe <tmccabe@bu.edu>
2019-04-13 13:49:09 -0400

    Working Ensemble forecast

---
+++ 0.2.1.1_Run_JAGS_Model.R
@@ -0,0 +1,104 @@
+##` @title run_jags_model     
+##` @param ensemble_name     The number of the met ensemble
+##` @param n.iter            Number of iterations passed to coda.samples 
+##` @param outfile           where the run mcmc output should be saved
+##` @param temp              A vector of tempurature values
+##` @param precip            a vector of precip values
+##` @param modis             a vector of fire area values
+##` @description             This function will forecast the model the number of days that we have GEFS data but no Modis data. Will save the output as .Rdata object
+library(rjags, lib.loc = "/share/pkg/r/3.5.2/install/lib64/R/library")
+source("/usr3/graduate/tmccabe/mccabete/Fire_forecast_509/scripts/tess_geo_fork/fire_area_forecast/01.2.1_data__helper_functions.R")
+
+run_JAGS_model <- function(ensemble_name = NA, n.iter = 500000, outfile = "/usr3/graduate/tmccabe/mccabete/Fire_forecast_509/output/mcmc/", temp, precip, modis){
+
+## Get Number of Days of availible data
+
+GEF_days <- get_days(data_type = "GEFS") # Number of days with Temp and Precip (how many days we can forcast into the future. We may want to limit so that convergence is easier. )
+modis_days <- get_days(data_type = "MOD14A2")
+forecast_days <- length(GEF_days) - length(modis_days)
+
+N <- length(GEF_days) # Assuming we only want to forecast days with met assosiated with it
+  
+#### Set up Model
+Fire_timeseries <-" model {
+ ### priors
+ beta_precip ~ dnorm(r_0, v_r)
+ beta_temp ~ dnorm(k_0, v_k)
+ #beta_IC ~ dnorm(I_0, S_0)
+ #tau_modis ~ dgamma(mod_1, mod_2)
+ sigma ~dgamma(s_1, s_2)
+ x[1] ~ dnorm(mu1, 1/sigma)
+
+ 
+ ###Observation error
+ for ( i in 1:N){
+ y_modis[i] ~ dpois(x[i])#, tau_modis) # modis
+ }
+ 
+ ### Process model for Fire
+ for(t in 2:N) {
+
+ mu[t] <- x[t-1]+ beta_precip*y[t] + beta_temp*y_2[t] #+beta_IC
+ x[t] ~ dnorm(mu[t], 1/sigma) 
+ 
+
+ }
+ }
+ "
+#### Add NA's to modis products
+modis <- c(modis, rep(NA, forecast_days))
+
+#### Fill in data 
+data<-list()
+data$y <- precip # Precip
+data$y_2 <- temp # Temp
+data$y_modis <- modis # modis 
+data$N<- N #total number of days. Included forcast_days
+
+### Priors
+data$r_0<- -3 ## Probably a negative relationship -- likely more influential than temperature 
+data$v_r<- 1/10 ## Unsure how much uncertainty 
+data$k_0<- 3 # Probably positive relationship with temperature
+data$v_k<- 0.5 # no large SD
+data$mu1<-172000000  # Modis Burn area from feb second. Feb second is too early to include in our anlysis. 
+#data$v_0<- 10 
+#data$mod_1 <- 10
+#data$mod_2 <- 1
+data$s_1 <- 10
+data$s_2 <- 1
+
+inits_tess<-list() #
+inits_tess[[1]] <- list( beta_precip = -1, beta_temp = 30)
+inits_tess[[2]] <-  list(beta_precip = 1, beta_temp = 10 )
+inits_tess[[3]] <-  list(beta_precip = 1, beta_temp = 1 )
+
+j.model   <- jags.model (file = textConnection(Fire_timeseries),
+                         data = data,
+                         inits = inits_tess,
+                         n.chains = 3)
+
+jags.out   <- coda.samples (model = j.model,
+                            variable.names = c("x"),
+                            n.iter = n.iter)
+GBR <- gelman.plot(jags.out)
+burnin <- GBR$last.iter[tail(which(apply(GBR$shrink[,,2]>1.05,1,any)),1)+1]
+if(is.na(burnin)){
+  warning("GBR !< 1.05. Model may have failed to converge")
+  jags.burn <- jags.out
+  
+  did_it_converge <- "convergence_failed_GBR_test" ## Some ensemble members might not converge. Flagging saved file, maybe we will want to re-run? 
+}else{
+  did_it_converge <- "convergence_passed_GBR_test"
+  jags.burn <- window(jags.out,start=burnin, extend = FALSE)
+}
+
+
+
+#### Save output
+  date_stamp <- Sys.time()
+  date_stamp <- format(date_stamp, "%Y%m%d")
+  file_name <- paste("/usr3/graduate/tmccabe/mccabete/Fire_forecast_509/output/mcmc/", date_stamp, ".", ensemble_name,".", did_it_converge, ".","JAGS_run.Rdata", sep = "")
+  save(jags.burn, file = file_name )
+}
+
+
---
+++ 0.3.1_Run_Ensemble.R
@@ -0,0 +1,46 @@
+
+
+
+library(rjags, lib.loc = "/share/pkg/r/3.5.2/install/lib64/R/library")
+source("/usr3/graduate/tmccabe/mccabete/Fire_forecast_509/scripts/tess_geo_fork/fire_area_forecast/01.2.1_data__helper_functions.R")
+source("/usr3/graduate/tmccabe/mccabete/Fire_forecast_509/scripts/tess_geo_fork/fire_area_forecast/0.2.1.1_Run_JAGS_Model.R")
+
+### Read in Modis data
+modis <- read.csv("/usr3/graduate/tmccabe/mccabete/Fire_forecast_509/data/MOD14A2/2019/MOD14A2.csv")
+modis_days <- get_days(data_type = "MOD14A2")
+dates <- format(as.POSIXct(modis$X1), "%Y%m%d")
+modis <- modis$X2[dates %in% modis_days]
+
+### Read in temp and precip ensembles
+met_days <- get_days(data_type = "GEFS")
+temp_list <- matrix(ncol = 21, nrow = length(met_days))
+precip_list <- matrix(ncol = 21, nrow = length(met_days))
+
+for (i in seq_along(met_days)){
+  file_path <- paste("/usr3/graduate/tmccabe/mccabete/Fire_forecast_509/data/GEFS", met_days[i], sep = "/" )
+  temp_name <- paste(met_days[i], "_full_ensemble_tempurature.csv", sep ="")
+  precip_name <- paste(met_days[i], "_full_ensemble_precipitation.csv", sep ="")
+  
+  temp_path <- paste (file_path, temp_name, sep = "/")
+  precip_path <- paste (file_path, precip_name, sep = "/")
+  temp_object <- read.csv(temp_path)
+  temp_object <- na.omit(temp_object[,-1])
+  precip_object <- read.csv(precip_path)
+  precip_object <- na.omit(precip_object[,-1])
+
+  ### Sumamrize to 8-day agggrigate (super basic)
+  for (j in 1:21){
+    precip_list[i,j] <- mean(precip_object[,j])
+    temp_list[i,j] <- max(temp_object[,j])
+  }
+}
+
+### Loop over ensemble members (21)
+ensemble_names <- names(temp_object)
+
+for (i in seq_along(ensemble_names)){
+  temp <- temp_list[, i]
+  precip <- precip_list[, i]
+  run_JAGS_model(ensemble_name = ensemble_names[i], temp = temp, precip = precip, modis= modis)
+}
+
