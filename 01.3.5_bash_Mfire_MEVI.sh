f8ec2118e4a5ed0020a5559d0b6dc5b7b6ae6c03
Yingtong <zhangyt@bu.edu>
2019-04-17 10:20:45 -0400

    make the scrip work for all available data automatically

--- 01.3.5_bash_Mfire_MEVI.sh
+++ 01.3.5_bash_Mfire_MEVI.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -l
+
 #$ -V
-#$ -l h_rt=96:00:00
-#$ -N extract_data.R
+#$ -l h_rt=24:00:00
+#$ -N extract_Mfire
 #$ -j y
 #$ -l mem_total=98G
-#$ -l mem_per_core=8G
-#$ -pe omp 8
+
 
 module purge
 module load R_earth/3.1.0
 Rscript /usr3/graduate/zhangyt/codes/fire_area_forecast/02.0_extract_data.R
+
