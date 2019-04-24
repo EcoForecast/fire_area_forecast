737131881a1a3ce9e3405d6e34374354c1fabcaa
Yingtong <zhangyt@bu.edu>
2019-04-24 09:34:37 -0400

    add VIIRS data and csv file is ready

--- 02.0_extract_data.R
+++ 02.0_extract_data.R
@@ -81,7 +81,7 @@ library(raster)
 
 # arguments provided as in and out folders
 infolder <- "/projectnb/dietzelab/tmccabe/mccabete/Fire_forecast_509/data/MOD14A2/2019/" 
-outfolder <- "/projectnb/dietzelab/tmccabe/mccabete/Fire_forecast_509/data/MOD14A2/"
+outfolder <- "/projectnb/dietzelab/tmccabe/mccabete/Fire_forecast_509/data/MOD14A2/2019/"
 
 ### here is a loop to read in all hdf datas and write out a single csv containing:
 # 1. the starting date of record
@@ -109,4 +109,52 @@ for (i in 1:length(fire.list)){
 }
 
 setwd(outfolder)
-write.csv(data.frame(out),"MOD14A2_2019.csv")
+write.csv(data.frame(out),"MOD14A2.csv")
+
+
+
+## ----------------------------------------------------------------------------------##
+### process on the VIIRS
+
+# arguments provided as in and out folders
+infolder <- "/projectnb/dietzelab/tmccabe/mccabete/Fire_forecast_509/data/VNP14A1/2019/" 
+outfolder <- "/projectnb/dietzelab/tmccabe/mccabete/Fire_forecast_509/data/VNP14A1/2019/"
+
+### here is a test. fire_data[26] stores number of fire pixels, fire_data[80] stores start date
+# fire_data <- paste0(infolder, "VNP14A1.A2019033.h29v12.001.2019035010349.h5")
+# gdalinfo(fire_data)[26]
+# gdalinfo(fire_data)[80]
+
+sub.folders <- list.dirs(infolder, recursive=TRUE)[-1]
+
+fire.files <- list()
+for (j in seq_along(sub.folders)) {
+  fire.files[[j]] <- dir(sub.folders[j],"\\.h5$")
+}
+#test <- lapply(evi.files, function(x) if(identical(x, character(0))) NA_character_ else x)
+fire.list <- fire.files[lapply(fire.files,length)>0]
+out <- matrix(NA,length(fire.list),2)
+
+for (i in 1:length(fire.list)){
+  image_name = fire.list[i][[1]]
+  image_dir = paste0(sub.folders[i], '/', image_name)  
+  
+  fireArea <- as.numeric(gsub("FirePix=","",gdalinfo(image_dir)[26])) * 1000^2
+  startDate <- as.Date(gsub("StartDate=","",gdalinfo(image_dir)[80]))
+  out[i,1] <- format(startDate,format="%Y-%m-%d")
+  out[i,2] <- fireArea
+  print(i)
+}
+
+for(i in 1:length(days)){
+  
+  directory <- paste0(0, days[i],'/', sep='')
+  file <- paste0(directory,dir(path = directory))
+  fireArea <- as.numeric(gsub("FirePix=","",gdalinfo(file)[26])) * 1000^2
+  startDate <- as.Date(gsub("StartDate=","",gdalinfo(file)[80]))
+  out[i,1] <- format(startDate,format="%Y-%m-%d")
+  out[i,2] <- fireArea
+}
+
+setwd(outfolder)
+write.csv(data.frame(out),"VNP14A1.csv")
